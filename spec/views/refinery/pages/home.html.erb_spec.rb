5571393769183f7929f96ba3f432ffff06dd775d
lanzhiheng <lanzhihengrj@gmail.com>
2019-01-08 00:12:38 +0800

    test: Add some test for home view.

---
+++ spec/views/refinery/pages/home.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe "refinery/pages/home" do
+  before(:all) do
+    assign(:pages, [
+             create(:home)
+           ])
+    assign(:posts, [
+             create(:docker, draft: false),
+             create(:ruby, draft: true)
+           ])
+    assign(:categories, [
+             create(:tech),
+             create(:live),
+           ])
+  end
+
+  it "displays categories in home page" do
+    render
+    expect(rendered).to match /<h6 class="card-title">Category<\/h6>/
+    expect(rendered).to match /href="#{refinery.blog_category_path(build(:tech))}"/
+    expect(rendered).to match /href="#{refinery.blog_category_path(build(:live))}"/
+  end
+end
