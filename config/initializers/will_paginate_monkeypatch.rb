ce0800be35a05b7a4953da81abe478c880793d15
lanzhiheng <lanzhihengrj@gmail.com>
2019-01-12 00:29:31 +0800

    feat: Adjust the stylesheet of pagination.

--- config/initializers/will_paginate_monkeypatch.rb
+++ config/initializers/will_paginate_monkeypatch.rb
@@ -3,11 +3,28 @@ require "will_paginate/view_helpers/action_view"
 module WillPaginate
   module ActionView
     class LinkRenderer < ViewHelpers::LinkRenderer
+      def page_number(page)
+        extra_class = page == current_page ? "disabled" : ""
+        tag(
+          :li,
+          link(page, page, rel: rel_value(page), class: "page-link"),
+          class: "page-item #{extra_class}"
+        )
+      end
+
+      def pagination
+        @options[:page_links] ? windowed_page_numbers : []
+      end
+
+      def html_container(html)
+        tag(:ul, html, container_attributes)
+      end
+
       def url(page)
         @base_url_params ||= begin
-          url_params = merge_get_params(default_url_params)
-          merge_optional_params(url_params)
-        end
+                               url_params = merge_get_params(default_url_params)
+                               merge_optional_params(url_params)
+                             end
 
         url_params = @base_url_params.dup
 
