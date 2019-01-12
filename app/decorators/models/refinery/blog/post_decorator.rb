46a5d1253ac64cca9ebd959f7d8f3882d75671a6
lanzhiheng <lanzhihengrj@gmail.com>
2019-01-12 09:06:33 +0800

    feat: use slug for post.

--- app/decorators/models/refinery/blog/post_decorator.rb
+++ app/decorators/models/refinery/blog/post_decorator.rb
@@ -3,6 +3,10 @@ Refinery::Blog::Post.class_eval do
     markdown_to_html(body)
   end
 
+  def should_generate_new_friendly_id?
+    will_save_change_to_attribute?(:custom_url) || will_save_change_to_attribute?(:title)
+  end
+
   private
   def markdown_to_html(markdown)
     @markdown_renderer ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, fenced_code_blocks: true)
