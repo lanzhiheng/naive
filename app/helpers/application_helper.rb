55c86fcd62782cd7a587a8a9a63df3a997c50615
lanzhiheng <lanzhihengrj@gmail.com>
2019-01-19 20:05:23 +0800

    feat: Remove the monkey patching.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,43 @@
+require "will_paginate/view_helpers/action_view"
+
+class MycustomLinkRenderer < WillPaginate::ActionView::LinkRenderer
+  def page_number(page)
+    extra_class = page == current_page ? "disabled" : ""
+    tag(
+      :li,
+      link(page, page, rel: rel_value(page), class: "page-link"),
+      class: "page-item #{extra_class}"
+    )
+  end
+
+  def pagination
+    @options[:page_links] ? windowed_page_numbers : []
+  end
+
+  def html_container(html)
+    tag(:ul, html, container_attributes)
+  end
+
+  def url(page)
+    @base_url_params ||= begin
+                           url_params = merge_get_params(default_url_params)
+                           merge_optional_params(url_params)
+                         end
+
+    url_params = @base_url_params.dup
+
+    # Remove the locale
+    url_params.delete(:locale)
+    add_current_page_param(url_params, page)
+
+    if /refinery/ === url_params[:controller]
+      @template.refinery.url_for(url_params.merge({ only_path: true }))
+    else
+      @template.url_for(url_params)
+    end
+  end
+end
+
 module ApplicationHelper
   def menu_list(current_path)
     Refinery::Page.fast_menu.map do |menu|
@@ -27,5 +67,16 @@ module ApplicationHelper
     )
 
     [fontawesome].join('').html_safe
+  end
+
+  # Custom the paginator
+  def will_paginate(collection = nil, options = {})
+    options, collection = collection, nil if collection.is_a? Hash
+    collection ||= infer_collection_from_controller
+
+    options = options.symbolize_keys
+    options[:renderer] ||= MycustomLinkRenderer
+
+    super(collection, options)
   end
 end
